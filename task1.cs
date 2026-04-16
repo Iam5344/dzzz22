f335ce559beaf0807fb4fb67e70c07b1faa63734
Arthas <gleban228777@gmail.com>
2026-04-16 18:09:53 +0300

    Update task1.cs

--- task1.cs
+++ task1.cs
@@ -1,161 +1,129 @@
-using Microsoft.EntityFrameworkCore;
-
-public class Game
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = null!;
-    public string Genre { get; set; } = null!;
-    public int MinPlayers { get; set; }
-    public int MaxPlayers { get; set; }
-    public List<Session> Sessions { get; set; } = new();
-}
-
-public class Member
-{
-    public int Id { get; set; }
-    public string FullName { get; set; } = null!;
-    public DateTime JoinDate { get; set; }
-    public List<MemberSession> MemberSessions { get; set; } = new();
-}
-
-public class Session
-{
-    public int Id { get; set; }
-    public int GameId { get; set; }
-    public Game Game { get; set; } = null!;
-    public DateTime Date { get; set; }
-    public int DurationMinutes { get; set; }
-    public List<MemberSession> MemberSessions { get; set; } = new();
-}
-
-public class MemberSession
-{
-    public int Id { get; set; }
-    public int MemberId { get; set; }
-    public Member Member { get; set; } = null!;
-    public int SessionId { get; set; }
-    public Session Session { get; set; } = null!;
-}
-
-public class AppDbContext : DbContext
-{
-    public DbSet<Game> Games { get; set; }
-    public DbSet<Member> Members { get; set; }
-    public DbSet<Session> Sessions { get; set; }
-    public DbSet<MemberSession> MembersSessions { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        options.UseSqlServer("Data Source=DESKTOP-8UTPR8Q\\IAM5344;Initial Catalog=BoardGamesDb;Integrated Security=True;Encrypt=False;");
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Game>()
-            .Property(g => g.Title)
-            .HasMaxLength(100)
-            .IsRequired();
-
-        modelBuilder.Entity<Game>()
-            .ToTable(t => t.HasCheckConstraint("CK_MinPlayers", "MinPlayers > 0"));
-
-        modelBuilder.Entity<Game>()
-            .ToTable(t => t.HasCheckConstraint("CK_MaxPlayers", "MaxPlayers > 0"));
-
-        modelBuilder.Entity<Game>()
-            .ToTable(t => t.HasCheckConstraint("CK_MinMax", "MinPlayers <= MaxPlayers"));
-
-        modelBuilder.Entity<Member>()
-            .Property(m => m.JoinDate)
-            .HasColumnType("datetime");
-
-        modelBuilder.Entity<Session>()
-            .Property(s => s.Date)
-            .HasColumnType("datetime");
-
-        modelBuilder.Entity<Game>()
-            .HasMany(g => g.Sessions)
-            .WithOne(s => s.Game)
-            .HasForeignKey(s => s.GameId);
-
-        modelBuilder.Entity<MemberSession>()
-            .HasOne(ms => ms.Member)
-            .WithMany(m => m.MemberSessions)
-            .HasForeignKey(ms => ms.MemberId);
-
-        modelBuilder.Entity<MemberSession>()
-            .HasOne(ms => ms.Session)
-            .WithMany(s => s.MemberSessions)
-            .HasForeignKey(ms => ms.SessionId);
-    }
-}
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 class Program
 {
-    static void Main()
+    [STAThread]
+    static void Main(string[] args)
     {
-        using var db = new AppDbContext();
-        db.Database.Migrate();
-
-        if (!db.Members.Any())
+        // Завдання 3 - Дочірній процес
+        if (args.Length == 3)
         {
-            var members = new List<Member>
-            {
-                new Member { FullName = "Олексій Коваль", JoinDate = new DateTime(2023, 1, 10) },
-                new Member { FullName = "Марія Бондар", JoinDate = new DateTime(2023, 2, 15) },
-                new Member { FullName = "Іван Шевченко", JoinDate = new DateTime(2023, 3, 20) },
-                new Member { FullName = "Анна Мельник", JoinDate = new DateTime(2023, 4, 5) },
-                new Member { FullName = "Петро Лисенко", JoinDate = new DateTime(2023, 5, 12) }
-            };
+            double a = double.Parse(args[0]);
+            double b = double.Parse(args[1]);
+            string op = args[2];
+            double result = 0;
 
-            var games = new List<Game>
+            switch (op)
             {
-                new Game { Title = "Catan", Genre = "Strategy", MinPlayers = 2, MaxPlayers = 4 },
-                new Game { Title = "Dixit", Genre = "Creative", MinPlayers = 3, MaxPlayers = 6 },
-                new Game { Title = "Pandemic", Genre = "Cooperative", MinPlayers = 2, MaxPlayers = 4 },
-                new Game { Title = "Ticket to Ride", Genre = "Strategy", MinPlayers = 2, MaxPlayers = 5 },
-                new Game { Title = "Codenames", Genre = "Party", MinPlayers = 4, MaxPlayers = 8 }
-            };
-
-            db.Members.AddRange(members);
-            db.Games.AddRange(games);
-            db.SaveChanges();
-
-            var rnd = new Random();
-            var sessions = new List<Session>();
-            for (int i = 0; i < 10; i++)
-            {
-                sessions.Add(new Session
-                {
-                    GameId = games[rnd.Next(games.Count)].Id,
-                    Date = DateTime.Now.AddDays(-rnd.Next(1, 100)),
-                    DurationMinutes = rnd.Next(30, 180)
-                });
+                case "+": result = a + b; break;
+                case "-": result = a - b; break;
+                case "*": result = a * b; break;
+                case "/": result = b != 0 ? a / b : 0; break;
             }
 
-            db.Sessions.AddRange(sessions);
-            db.SaveChanges();
-
-            foreach (var session in sessions)
-            {
-                var shuffled = members.OrderBy(_ => rnd.Next()).Take(rnd.Next(2, 5)).ToList();
-                foreach (var member in shuffled)
-                {
-                    db.MembersSessions.Add(new MemberSession
-                    {
-                        MemberId = member.Id,
-                        SessionId = session.Id
-                    });
-                }
-            }
-
-            db.SaveChanges();
-            Console.WriteLine("БД заповнено.");
+            Console.WriteLine($"Аргументи: {a} {op} {b}");
+            Console.WriteLine($"Результат: {result}");
+            Console.ReadKey();
+            return;
         }
-        else
-        {
-            Console.WriteLine("БД вже заповнена.");
-        }
+
+        Application.Run(new Form1());
     }
 }
-Update-Database
+
+public class Form1 : Form
+{
+    Button btnTask1 = new Button() { Text = "Завдання 1", Left = 10, Top = 10, Width = 150 };
+    Button btnTask2Wait = new Button() { Text = "Завдання 2 - Чекати", Left = 10, Top = 50, Width = 150 };
+    Button btnTask2Kill = new Button() { Text = "Завдання 2 - Завершити", Left = 10, Top = 90, Width = 150 };
+    Button btnTask3 = new Button() { Text = "Завдання 3", Left = 10, Top = 130, Width = 150 };
+    TextBox txtNum1 = new TextBox() { Left = 170, Top = 130, Width = 60, Text = "7" };
+    TextBox txtNum2 = new TextBox() { Left = 240, Top = 130, Width = 60, Text = "3" };
+    ComboBox cmbOp = new ComboBox() { Left = 310, Top = 130, Width = 60, DropDownStyle = ComboBoxStyle.DropDownList };
+    Label lblResult = new Label() { Left = 10, Top = 200, Width = 400, AutoSize = false, Height = 60 };
+
+    Process childProcess;
+
+    public Form1()
+    {
+        this.Text = "Батьківський процес";
+        this.Width = 450;
+        this.Height = 320;
+
+        cmbOp.Items.AddRange(new string[] { "+", "-", "*", "/" });
+        cmbOp.SelectedIndex = 0;
+
+        this.Controls.Add(btnTask1);
+        this.Controls.Add(btnTask2Wait);
+        this.Controls.Add(btnTask2Kill);
+        this.Controls.Add(btnTask3);
+        this.Controls.Add(txtNum1);
+        this.Controls.Add(txtNum2);
+        this.Controls.Add(cmbOp);
+        this.Controls.Add(lblResult);
+
+        btnTask1.Click += (s, e) =>
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "notepad.exe";
+            proc.Start();
+
+            Console.WriteLine("Процес розпочато: " + proc.ProcessName);
+
+            proc.WaitForExit();
+
+            Console.WriteLine("Процес завершився з кодом: " + proc.ExitCode);
+            lblResult.Text = $"Завдання 1\nПроцес завершився з кодом: {proc.ExitCode}";
+        };
+
+        btnTask2Wait.Click += (s, e) =>
+        {
+            childProcess = new Process();
+            childProcess.StartInfo.FileName = "notepad.exe";
+            childProcess.Start();
+
+            lblResult.Text = "Завдання 2\nЧекаємо завершення процесу...";
+
+            childProcess.WaitForExit();
+
+            lblResult.Text = $"Завдання 2\nПроцес завершився з кодом: {childProcess.ExitCode}";
+        };
+
+        btnTask2Kill.Click += (s, e) =>
+        {
+            if (childProcess == null || childProcess.HasExited)
+            {
+                childProcess = new Process();
+                childProcess.StartInfo.FileName = "notepad.exe";
+                childProcess.Start();
+                lblResult.Text = "Завдання 2\nПроцес запущено.";
+                return;
+            }
+
+            childProcess.CloseMainWindow();
+
+            if (!childProcess.WaitForExit(2000))
+            {
+                childProcess.Kill();
+            }
+
+            childProcess.Dispose();
+            lblResult.Text = "Завдання 2\nПроцес примусово завершено.";
+        };
+
+        btnTask3.Click += (s, e) =>
+        {
+            string num1 = txtNum1.Text;
+            string num2 = txtNum2.Text;
+            string op = cmbOp.SelectedItem.ToString();
+
+            Process proc = new Process();
+            proc.StartInfo.FileName = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+            proc.StartInfo.Arguments = $"{num1} {num2} {op}";
+            proc.StartInfo.UseShellExecute = true;
+            proc.Start();
+        };
+    }
+}
