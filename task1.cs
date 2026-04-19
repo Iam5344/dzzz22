47fe2d55d08e1ec3b00d888d1c9f423cee5d6fad
Arthas <gleban228777@gmail.com>
2026-04-19 19:02:48 +0300

    Implement multi-threading for number generation and calculations
    Refactor the program to generate random numbers and calculate max, min, and average in separate threads.

--- task1.cs
+++ task1.cs
@@ -1,128 +1,63 @@
 using System;
-using System.Diagnostics;
-using System.Windows.Forms;
+using System.Threading;
 
-class Program
+int[] numbers = new int[1000];
+int max = 0, min = 0;
+double avg = 0;
+
+ManualResetEvent mre = new ManualResetEvent(false);
+
+Thread generator = new Thread(() =>
 {
-    [STAThread]
-    static void Main(string[] args)
-    {
-        if (args.Length == 3)
-        {
-            double a = double.Parse(args[0]);
-            double b = double.Parse(args[1]);
-            string op = args[2];
-            double result = 0;
+    Console.WriteLine("Генерація чисел...");
+    Random rnd = new Random();
+    for (int i = 0; i < 1000; i++)
+        numbers[i] = rnd.Next(0, 5000);
+    Console.WriteLine("Генерацію завершено");
+    mre.Set();
+});
 
-            switch (op)
-            {
-                case "+": result = a + b; break;
-                case "-": result = a - b; break;
-                case "*": result = a * b; break;
-                case "/": result = b != 0 ? a / b : 0; break;
-            }
-
-            Console.WriteLine($"Аргументи: {a} {op} {b}");
-            Console.WriteLine($"Результат: {result}");
-            Console.ReadKey();
-            return;
-        }
-
-        Application.Run(new Form1());
-    }
-}
-
-public class Form1 : Form
+Thread t1 = new Thread(() =>
 {
-    Button btnTask1 = new Button() { Text = "Завдання 1", Left = 10, Top = 10, Width = 150 };
-    Button btnTask2Wait = new Button() { Text = "Завдання 2 - Чекати", Left = 10, Top = 50, Width = 150 };
-    Button btnTask2Kill = new Button() { Text = "Завдання 2 - Завершити", Left = 10, Top = 90, Width = 150 };
-    Button btnTask3 = new Button() { Text = "Завдання 3", Left = 10, Top = 130, Width = 150 };
-    TextBox txtNum1 = new TextBox() { Left = 170, Top = 130, Width = 60, Text = "7" };
-    TextBox txtNum2 = new TextBox() { Left = 240, Top = 130, Width = 60, Text = "3" };
-    ComboBox cmbOp = new ComboBox() { Left = 310, Top = 130, Width = 60, DropDownStyle = ComboBoxStyle.DropDownList };
-    Label lblResult = new Label() { Left = 10, Top = 200, Width = 400, AutoSize = false, Height = 60 };
+    Console.WriteLine("Потік 1 очікує...");
+    mre.WaitOne();
+    Console.WriteLine("Потік 1 шукає максимум");
+    max = numbers[0];
+    foreach (int n in numbers)
+        if (n > max) max = n;
+    Console.WriteLine("Максимум: " + max);
+});
 
-    Process childProcess;
+Thread t2 = new Thread(() =>
+{
+    Console.WriteLine("Потік 2 очікує");
+    mre.WaitOne();
+    Console.WriteLine("Потік 2 шукає мінімум");
+    min = numbers[0];
+    foreach (int n in numbers)
+        if (n < min) min = n;
+    Console.WriteLine("Мінімум: " + min);
+});
 
-    public Form1()
-    {
-        this.Text = "Батьківський процес";
-        this.Width = 450;
-        this.Height = 320;
+Thread t3 = new Thread(() =>
+{
+    Console.WriteLine("Потік 3 очікує");
+    mre.WaitOne();
+    Console.WriteLine("Потік 3 обчислює середнє");
+    long sum = 0;
+    foreach (int n in numbers)
+        sum += n;
+    avg = (double)sum / numbers.Length;
+    Console.WriteLine("Середнє: " + avg);
+});
 
-        cmbOp.Items.AddRange(new string[] { "+", "-", "*", "/" });
-        cmbOp.SelectedIndex = 0;
+t1.Start();
+t2.Start();
+t3.Start();
+generator.Start();
 
-        this.Controls.Add(btnTask1);
-        this.Controls.Add(btnTask2Wait);
-        this.Controls.Add(btnTask2Kill);
-        this.Controls.Add(btnTask3);
-        this.Controls.Add(txtNum1);
-        this.Controls.Add(txtNum2);
-        this.Controls.Add(cmbOp);
-        this.Controls.Add(lblResult);
+generator.Join();
+t1.Join();
+t2.Join();
+t3.Join();
 
-        btnTask1.Click += (s, e) =>
-        {
-            Process proc = new Process();
-            proc.StartInfo.FileName = "notepad.exe";
-            proc.Start();
-
-            Console.WriteLine("Процес розпочато: " + proc.ProcessName);
-
-            proc.WaitForExit();
-
-            Console.WriteLine("Процес завершився з кодом: " + proc.ExitCode);
-            lblResult.Text = $"Завдання 1\nПроцес завершився з кодом: {proc.ExitCode}";
-        };
-
-        btnTask2Wait.Click += (s, e) =>
-        {
-            childProcess = new Process();
-            childProcess.StartInfo.FileName = "notepad.exe";
-            childProcess.Start();
-
-            lblResult.Text = "Завдання 2\nЧекаємо завершення процесу...";
-
-            childProcess.WaitForExit();
-
-            lblResult.Text = $"Завдання 2\nПроцес завершився з кодом: {childProcess.ExitCode}";
-        };
-
-        btnTask2Kill.Click += (s, e) =>
-        {
-            if (childProcess == null || childProcess.HasExited)
-            {
-                childProcess = new Process();
-                childProcess.StartInfo.FileName = "notepad.exe";
-                childProcess.Start();
-                lblResult.Text = "Завдання 2\nПроцес запущено.";
-                return;
-            }
-
-            childProcess.CloseMainWindow();
-
-            if (!childProcess.WaitForExit(2000))
-            {
-                childProcess.Kill();
-            }
-
-            childProcess.Dispose();
-            lblResult.Text = "Завдання 2\nПроцес примусово завершено.";
-        };
-
-        btnTask3.Click += (s, e) =>
-        {
-            string num1 = txtNum1.Text;
-            string num2 = txtNum2.Text;
-            string op = cmbOp.SelectedItem.ToString();
-
-            Process proc = new Process();
-            proc.StartInfo.FileName = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
-            proc.StartInfo.Arguments = $"{num1} {num2} {op}";
-            proc.StartInfo.UseShellExecute = true;
-            proc.Start();
-        };
-    }
-}
